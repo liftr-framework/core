0333ef921c43d914a1e60d290778179ececf4375
Faris Tangastani <ftangastani@gmail.com>
2019-08-13 15:13:28 +0200

    feat: added global and functions/ still need to remove class

--- src/core.ts
+++ src/core.ts
@@ -1,11 +1,47 @@
-import { Application, Router } from 'express';
-import { LiftrDocs } from 'liftr-docs';
+import { Application, Router, RequestHandler } from 'express';
+import { LiftrDocs } from '@liftr/docs';
 import { SwaggerRequestBody, SwaggerDescriptionInfo, SwaggerServers } from './interfaces';
+import { Schema } from 'joi';
 
+declare global {
+    namespace Express {
+      interface Request  {
+        session?: Session;
+        validate(data: string| Object, schema: Schema): Promise<any>;
+      }
+      interface SessionData {
+        [key: string]: any;
+      }
+  
+      interface Session extends SessionData {
+        isValid(issuer: string): boolean;
+        hasFunctionality(functionalityId: string): boolean;
+        get(attr: string): any;
+        logout(res: Express.Request, cookieDomain: string): void;
+        fetch(id: string, logging: any): Promise<any>;
+        destroy(id: string, res: Response, cookieDomain: string, logging: any): Promise<any>;
+      }
+    }
+  }
+
+// declare module 'express-serve-static-core' {
+
+//     interface Router extends {
+//         post: Express;
+//     }
+//     interface Request {
+//       myField?: string
+//     }
+//     interface Response {
+//       myField?: string
+//     }
+//   }
 /**
  * The Liftr class contains methods to run a Liftr Server
  * See the documentation for a clear overview of what each one does.
  */
+
+ // TODO: Remove class and make it function based (running class is too much overhead)
 export class Liftr {
     routes: AppRouter[];
     app: Application;
@@ -14,13 +50,12 @@ export class Liftr {
         this.app = app;
     }
 
-
     /**
      * The server method runs the Application that is passed based on the port and configuration already set
-     * @type {Application}
-     * @param app  An express Application is passed here to start the server
+     * @param {Application} app  An express Application is passed here to start the server
      */
-    static server(app: Application) {
+    public server(app: Application) {
+
        return app.listen(app.get('port'), () => {
             console.log(
                 'App is running on http://localhost:%d in %s mode',
@@ -32,31 +67,63 @@ export class Liftr {
 
     /**
      * The setRoutes method loops through the routes and sets them on the Application server
-     * @type {AppRouter}
-     * @param routes The routes setup in the LiftrRoutingModule are passed here
-     * @type {Application}
-     * @param app  An express Application is passed here to start the server
+     * @param {AppRouter[]} routes The routes setup in the LiftrRoutingModule are passed here
+     * @param {Application} app  An express Application is passed here to start the server
      */
-    static setRoutes(routes: AppRouter[], app: Application) {
+    public setRoutes(routes: AppRouter[], app: Application) {
         return routes.forEach((route: AppRouter) => app.use(route.path, route.middleware, route.handler));
     }
 
     /**
      * The useDocs runs LiftrDocs under the hood.
      * Refer to the documentation for more details https://github.com/farisT/liftr-docs
-     * @type {Application}
-     * @param app  An express Application is passed here to start the server
-     * @type {AppRouter}
-     * @param routes The routes setup in the LiftrRoutingModule are passed here
-     * @type {SwaggerDescriptions}
-     * @param swaggerDescriptions
-     * @type {SwaggerResponses}
-     * @param swaggerResponses
+     * @param {Application} app  An express Application is passed here to start the server
+     * @param {AppRouter[]} routes The routes setup in the LiftrRoutingModule are passed here
+     * @param  {SwaggerDescriptions} swaggerDescriptions
+     * @param {SwaggerResponses} swaggerResponses
      */
 
-    static useDocs(app: Application, routes:AppRouter[], swaggerDescriptions: SwaggerDescriptions, swaggerResponses: SwaggerResponses) {
+    public useDocs(app: Application, routes:AppRouter[], swaggerDescriptions: SwaggerDescriptions, swaggerResponses: SwaggerResponses) {
         return app.use('/docs', LiftrDocs(routes, swaggerDescriptions, swaggerResponses));
+
     }
+}
+
+/**
+ * The setRoutes method loops through the routes and sets them on the Application server
+ * @param {AppRouter[]} routes The routes setup in the LiftrRoutingModule are passed here
+ * @param {Application} app  An express Application is passed here to start the server
+*/
+function setRoutes(app: Application, routes: AppRouter[]) {
+    return routes.forEach((route: AppRouter) => app.use(route.path, route.middleware, route.handler));
+}
+
+/**
+ * The useDocs runs LiftrDocs under the hood.
+ * Refer to the documentation for more details https://github.com/farisT/liftr-docs
+ * @param {Application} app  An express Application is passed here to start the server
+ * @param {AppRouter[]} routes The routes setup in the LiftrRoutingModule are passed here
+ * @param  {SwaggerDescriptions} swaggerDescriptions
+ * @param {SwaggerResponses} swaggerResponses
+ */
+export function useDocs(
+    app: Application, 
+    routes:AppRouter[],
+    swaggerDescriptions: SwaggerDescriptions,
+    swaggerResponses: SwaggerResponses
+    ) {
+    return app.use('/docs', LiftrDocs(routes, swaggerDescriptions, swaggerResponses));
+}
+
+export function server(app: Application, routes: AppRouter[]) {
+    setRoutes(app, routes);
+    return app.listen(app.get('port'), () => {
+        console.log(
+            'App is running on http://localhost:%d in %s mode',
+            app.get('port'),
+            app.get('env'),
+        );
+    });
 }
 
 /**
